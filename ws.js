09a2ad0ac90ac7a37d866f6796807c292faa053c
Chris Timms <chriscotimms@gmail.com>
2023-03-22 16:11:41 +0000

    working focus tab, enter key activates focused element, display none applied to hidden menus

--- ws.js
+++ ws.js
@@ -1,24 +1,74 @@
 const sidebar1 = document.querySelector('.sidebar1');
 const sidebar2 = document.querySelector('.sidebar2');
 const main = document.querySelector('.main');
+//const header = document.querySelector('.header')
+//const btns = document.querySelectorl('btns')
+//console.log(btns.classList);
 
+// variable for currently focused element
+let focusNow = document.getElementById(nav_menu);
 
-document.querySelector('#access_menu').onclick = function () {
-  if (sidebar1.classList.contains('side_bigger')) {
-    sidebar1.classList.toggle('side_bigger');
-  } else {
-    main.classList.toggle('main_smaller');
+document.addEventListener('focusin', function() {
+  focusNow = document.activeElement;
+  //console.log('focused: ', document.activeElement);
+  console.log(focusNow);// console log focus elements
+}, true);
+
+window.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the focused element with a click
+    focusNow.click();
   }
-  sidebar2.classList.toggle('side_bigger');
+});
+
+
+// add and remove display none class to navigation and access menus to make sure tab navigation isn't overwhlemed 
+function displayRemove(a) {
+  a.classList.remove('display_none');
 };
 
-document.querySelector('#nav_menu').onclick = function () {
-  if (sidebar2.classList.contains('side_bigger')) {
-    sidebar2.classList.toggle('side_bigger');
+function displayAdd(a) {
+  a.classList.add('display_none');
+};
+
+//access menu toggle
+document.querySelector('#access_menu').onclick = function () {
+  if (sidebar1.classList.contains('side_bigger')) { //if other sidebar is open close it
+    sidebar1.classList.toggle('side_bigger');
+    setTimeout(displayAdd, 500, sidebar1);
   } else {
-    main.classList.toggle('main_smaller');
+    main.classList.toggle('main_smaller');//make main section smaller only if neither side is already open
+  }
+  sidebar2.classList.toggle('side_bigger');
+  if (sidebar2.classList.contains('display_none')) {
+    sidebar2.classList.remove('display_none');
+    } else {
+      setTimeout(displayAdd, 500, sidebar2);
+    }
+    setTimeout(displayAdd, 500, sidebar1);
+
+  //sidebar2.classList.toggle('display_none');
+  //setTimeout(displayAdd, 500, sidebar1);
+};
+
+// navigation menu toggle
+document.querySelector('#nav_menu').onclick = function () {
+  if (sidebar2.classList.contains('side_bigger')) { //if other sidebar is open close it  
+    sidebar2.classList.toggle('side_bigger');
+    setTimeout(displayAdd, 500, sidebar2);
+  } else {
+    main.classList.toggle('main_smaller'); //make main section smaller only if neither side is already open
   }
   sidebar1.classList.toggle('side_bigger');
+  if (sidebar1.classList.contains('display_none')) {
+  sidebar1.classList.remove('display_none');
+  } else {
+    setTimeout(displayAdd, 500, sidebar1);
+  }
+  setTimeout(displayAdd, 500, sidebar2);
 };
 
 
@@ -32,10 +82,10 @@ const changeFont1 = (a) => {
   main.classList.toggle(a.target.id);
 }; 
 
+
 oneX.addEventListener('click', changeFont1);
 oneandhalfX.addEventListener('click', changeFont1);
 twoX.addEventListener('click', changeFont1);
-
 
 
 //access menu - colour scheme
@@ -50,7 +100,37 @@ const bL = document.querySelector('.theme3alt');
 
 const changeColour = (a) => {
   main.classList.remove('whiteblack', 'blackwhite', 'theme1', 'theme1alt', 'theme2', 'theme2alt', 'theme3', 'theme3alt');
+  //header.classList.remove('whiteblack', 'blackwhite', 'theme1', 'theme1alt', 'theme2', 'theme2alt', 'theme3', 'theme3alt');
+  //btns.classList.remove('whiteblack', 'blackwhite', 'theme1', 'theme1alt', 'theme2', 'theme2alt', 'theme3', 'theme3alt');
   main.classList.toggle(a.target.id);
+  //header.classList.toggle(a.target.id);
+  /*switch (a.target.id) {
+    case 'whiteblack':
+      btns.classList.toggle('blackwhite');
+      break;
+    case 'blackwhite':
+      btns.classList.toggle('whiteblack');
+      break;
+    case 'theme1':
+      console.log('acme')
+      break;
+    case 'theme1alt':
+      console.log('lora')
+      break;
+    case 'theme2':
+      console.log('acme')
+      break;
+    case 'theme2alt':
+      console.log('lora')
+      break;
+    case 'theme3':
+      console.log('acme')
+      break;
+    case 'theme3alt':
+      console.log('lora')
+      break;
+
+  }*/
 }; 
 
 wB.addEventListener("click", changeColour);
@@ -63,83 +143,24 @@ lB.addEventListener("click", changeColour);
 bL.addEventListener("click", changeColour);
 
 
- 
 //access menu - font family
 const acme = document.querySelector('.acme');
-const asapCon = document.querySelector('.asapCon');
+const lora = document.querySelector('.lora');
 const nunito = document.querySelector('.nunito');
 const signika = document.querySelector('.signika');
-const staatliches = document.querySelector('.staatliches');
+const mono = document.querySelector('.mono');
 
 const changeFF = (a) => {
-  main.classList.remove('acme', 'asapCon', 'nunito', 'signika', 'staatliches');
+  main.classList.remove('acme', 'lora', 'nunito', 'signika', 'mono');
   main.classList.toggle(a.target.id);
 }; 
 
 acme.addEventListener("click", changeFF);
-asapCon.addEventListener("click", changeFF);
+lora.addEventListener("click", changeFF);
 nunito.addEventListener("click", changeFF);
 signika.addEventListener("click", changeFF);
-staatliches.addEventListener("click", changeFF);
+mono.addEventListener("click", changeFF);
 
 
 
 
-//function changeColor(color) {
-//for (let i = 0; i < note.length; i++) {
-//note[i].style.backgroundColor = color;
-//note[i].style.color = 'red';
-//}
-//}
-
-
-const flex = document.getElementById('flexBody');
-const flexChoices = ['row', 'row-reverse', 'column', 'column-reverse'];
-console.log(flex);
-
-
-function column() {
-    flex.style['flex-direction'] = flexChoices[2];
-    }
-
-function row() {
-    flex.style['flex-direction'] = flexChoices[0];
-    }
-
-
-
-
-/*
-
-let value = document.getElementsByTagName("article");
-console.log(value);
-
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
-
-*/
-
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
